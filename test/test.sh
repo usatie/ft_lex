a83b461ceeaa3de4eb2eff8d791b2b8b12b9cc2b
Shun Usami <usatie@gmail.com>
2025-02-21 17:27:47 -0800

    Fix the test script to use the correct path to ft_lex

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 lex -o lexcmp.yy.c scanner.l 
 clang -o scanner_cmp lexcmp.yy.c -ll
-ft_lex scanner.l
+../ft_lex scanner.l
 clang -o scanner_ft lex.yy.c -ll
 diff -u lexcmp.yy.c lex.yy.c
 echo "42+1337+(21*19)" | ./scanner_cmp | tee cmp.txt
