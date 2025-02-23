946b5b708ba66e907c32df5eb55b3cc1cf9af38e
Shun Usami <usatie@gmail.com>
2025-02-22 17:31:54 -0800

    [Refactor] write to lex.yy.c using a function

--- src/main.c
+++ src/main.c
@@ -3,32 +3,41 @@
 #include <string.h>
 #include <unistd.h>
 
+#define DEFAULT_OUTPUT_FILENAME "lex.yy.c"
+#define DUMMY_LEX_YY_C                                                         \
+  "#include <stdio.h>\n"                                                       \
+  "#include <string.h>\n"                                                      \
+  "#include <stdlib.h>\n"                                                      \
+  "\n"                                                                         \
+  "int main(void) {\n"                                                         \
+  "\tprintf(\"Hello, world!\\n\");\n"                                          \
+  "\treturn 0;\n"                                                              \
+  "}\n"
+
+int write_to_lex_yy_c(const char *src, const char *filename) {
+  FILE *fp = fopen(filename, "w");
+  if (fp == NULL) {
+    perror("fopen");
+    return -1;
+  }
+  size_t src_len = strlen(src);
+  if (fwrite(src, sizeof(char), src_len, fp) < src_len) {
+    perror("fwrite");
+    fclose(fp);
+    return -1;
+  }
+  fclose(fp);
+  return 0;
+}
+
 int main(void) {
-	char *src = "#include <stdio.h>\n"
-		"#include <string.h>\n"
-		"#include <stdlib.h>\n"
-		"\n"
-		"int main(void) {\n"
-		"\tprintf(\"Hello, world!\\n\");\n"
-		"\treturn 0;\n"
-		"}\n";
-	char *filename = "lex.yy.c";
-	int fd = open(filename, O_CREAT | O_WRONLY, 0644);
-	if (fd < 0) {
-		perror("open");
-		return 1;
-	}
-	size_t src_len = strlen(src);
-	size_t written = 0;
-	while (written < src_len) {
-		ssize_t n = write(fd, src + written, src_len - written);
-		if (n < 0) {
-			perror("write");
-			close(fd);
-			return 1;
-		}
-		written += (size_t)n;
-	}
-	close(fd);
-	return 0;
+  // TODO: Read, tokenize, parse xxx.l to generate table
+  // TODO: Generate NFA from table
+  // TODO: Generate DFA from NFA
+  // TODO: Generate code from DFA
+  // TODO: Write to lex.yy.c
+  if (write_to_lex_yy_c(DUMMY_LEX_YY_C, DEFAULT_OUTPUT_FILENAME) < 0) {
+    return -1;
+  }
+  return 0;
 }
