2a01d452d6c1bbe9cb080d5b39248158961dc598
Shun Usami <usatie@gmail.com>
2025-03-01 10:21:34 -0800

    Implement NFA matching algorithm code in C
    - Basically copying the code from Russ Cox's article "Regular Expression Matching Can Be Simple And Fast"

--- playground/regex.c
+++ playground/regex.c
@@ -4,12 +4,120 @@
 #include <string.h>
 #include <unistd.h> // STDERR_FILENO
 
+/*
+ * Convert infix regexp re to postfix notation.
+ * Insert . as explicit concatenation operator.
+ * Cheesy parser, return static buffer.
+ */
+char *re2post(char *re) {
+  int nalt, natom;
+  static char buf[8000] = {};
+  char *dst;
+  // Q. what's nalt and natom?
+  struct {
+    int nalt;
+    int natom;
+  } paren[100], *p;
+
+  p = paren;
+  dst = buf;
+  nalt = 0;
+  natom = 0;
+  // The maximum length of postfix form is (2 * len - 1),
+  // when re is plain text without special characters "abc"
+  // because it will converted to "abc.."
+  if (strlen(re) >= sizeof(buf) / 2) {
+    return NULL;
+  }
+  for (; *re; re++) {
+    switch (*re) {
+    case '(':
+      if (natom > 1) {
+        --natom;
+        *dst++ = '.';
+      }
+      if (p >= paren + 100) {
+        return NULL; // Q. what's this? paren buffer size is 100
+      }
+      // Stash nalt, natom when a parenthesis open
+      p->nalt = nalt;
+      p->natom = natom;
+      p++;
+      // Start new nalt, natom counting
+      nalt = 0;
+      natom = 0;
+      break;
+    case '|':
+      if (natom == 0) {
+        return NULL; // natom is ...?
+      }
+      while (--natom > 0) {
+        *dst++ = '.';
+      }
+      nalt++; // number of alternate
+      break;
+    case ')':
+      if (p == paren) {
+        return NULL;
+      }
+      if (natom == 0) { // what is natom?
+        return NULL;
+      }
+      while (--natom > 0) {
+        *dst++ = '.';
+      }
+      for (; nalt > 0; nalt--) {
+        *dst++ = '|';
+      }
+      --p;
+      // Restore nalt, natom
+      nalt = p->nalt;
+      natom = p->natom;
+      // Increment natom, because (...) of itself is natom
+      natom++;
+      break;
+    case '*':
+    case '+':
+    case '?':
+      if (natom == 0) { // natom is an atomic element such as normal
+                        // characters and concatenated nodes
+        return NULL;
+      }
+      *dst++ = *re;
+      break;
+    default:
+      if (natom > 1) {
+        --natom;
+        *dst++ = '.';
+      }
+      *dst++ = *re;
+      natom++;
+      break;
+    }
+    printf("*re = %c, nalt = %d, natom = %d, buf = %s\n", *re, nalt, natom,
+           buf);
+  }
+  // All the parenthesis must be closed
+  if (p != paren) {
+    return NULL;
+  }
+  while (--natom > 0) {
+    *dst++ = '.';
+  }
+  for (; nalt > 0; nalt--) {
+    *dst++ = '|';
+  }
+  *dst = 0;
+  return buf;
+}
+
 enum { Match = 256, Split = 257 };
 typedef struct State State;
 struct State {
   int c;
   State *out;
   State *out1;
+  int lastlist;
 };
 
 State matchstate = {Match}; // matching state
@@ -24,6 +132,7 @@ State *state(int c, State *out, State *out1) {
   s->c = c;
   s->out = out;
   s->out1 = out1;
+  s->lastlist = 0;
   return s;
 }
 
@@ -182,4 +291,119 @@ State *post2nfa(char *postfix) {
 #undef push
 }
 
-int main(void) {}
+typedef struct List List;
+struct List {
+  State **s;
+  int n;
+};
+
+List l1, l2;
+static int listid;
+
+void addstate(List *, State *);
+void step(List *, int, List *);
+
+/* Compute initial state list */
+List *startlist(State *start, List *l) {
+  l->n = 0;
+  listid++;
+  addstate(l, start);
+  return l;
+}
+
+/* Check whether state list contains a match. */
+int ismatch(List *l) {
+  int i;
+
+  for (i = 0; i < l->n; i++) {
+    if (l->s[i] == &matchstate) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+/* Add s to l, following unlabeled arrows. */
+void addstate(List *l, State *s) {
+  if (s == NULL || s->lastlist == listid) {
+    return;
+  }
+  s->lastlist = listid;
+  if (s->c == Split) {
+    /* follow unlabled arrows */
+    addstate(l, s->out);
+    addstate(l, s->out1);
+    return;
+  }
+  l->s[l->n++] = s;
+}
+
+/*
+ * Step the NFA from the states in clist
+ * past the character c,
+ * to create next NFA state set nlist.
+ */
+void step(List *clist, int c, List *nlist) {
+  int i;
+  State *s;
+  listid++;
+  nlist->n = 0;
+  for (i = 0; i < clist->n; i++) {
+    s = clist->s[i];
+    if (s->c == c) {
+      // TODO: Implement wildcard
+      addstate(nlist, s->out);
+    }
+  }
+}
+
+/* Run NFA to determine whether it matches s. */
+int match(State *start, char *s) {
+  int i, c;
+  List *clist, *nlist, *t;
+
+  clist = startlist(start, &l1);
+  nlist = &l2;
+  for (; *s; s++) {
+    c = *s & 0xFF; // to prevent sign extension
+    step(clist, c, nlist);
+    t = clist;
+    clist = nlist;
+    nlist = t; /* swap clist, nlist */
+  }
+  // Q. There isn't early exit in the for loop?
+  // Q. Match against whole string?
+  return ismatch(clist);
+}
+
+int main(int argc, char **argv) {
+  int i;
+  char *post;
+  State *start;
+
+  if (argc < 3) {
+    fprintf(stderr, "usage: nfa regexp string...\n");
+    return 1;
+  }
+
+  post = re2post(argv[1]);
+  if (post == NULL) {
+    fprintf(stderr, "bad regexp %s\n", argv[1]);
+    return 1;
+  }
+
+  start = post2nfa(post);
+  if (start == NULL) {
+    fprintf(stderr, "error in post2nfa %s\n", post);
+    return 1;
+  }
+
+  l1.s = malloc(nstate * sizeof(l1.s[0]));
+  l2.s = malloc(nstate * sizeof(l2.s[0]));
+  for (i = 2; i < argc; i++) {
+    if (match(start, argv[i])) {
+      printf("%s\n", argv[i]);
+    }
+  }
+  return 0;
+}
